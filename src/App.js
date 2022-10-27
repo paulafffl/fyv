bad92840d34e2aa47297d03c4a5a2e845539ea8e
Paula Lima <paula.lima@mvfglobal.com>
2022-10-27 10:58:48 +0100

    Simplify background

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./Styles.scss";
 import { About } from "./Components/about";
 import { Navbar } from "./Components/navbar";
 import { Header } from "./Components/header";
-import { Bckg } from "./Components/bckg";
 import { Section } from "./Components/section";
 import { Footer } from "./Components/footer";
 import { contentPosts } from "./Images/Thumbnails/content";
@@ -11,15 +10,12 @@ import { contentManifesto } from "./Images/Thumbnails/content";
 function App() {
 	return (
 		<div>
-			<Bckg />
-			<div className="content">
-				<Navbar />
-				<Header id="header" />
-				<Section id="manifesto" title={"manifesto"} content={contentManifesto} />
-				<Section id="posts" title={"posts"} content={contentPosts} />
-				<About id="about" />
-				<Footer id="support" />
-			</div >
+			<Navbar />
+			<Header id="header" />
+			<Section id="manifesto" title={"manifesto"} content={contentManifesto} />
+			<Section id="posts" title={"posts"} content={contentPosts} />
+			<About id="about" />
+			<Footer id="support" />
 		</div >
 	);
 }
