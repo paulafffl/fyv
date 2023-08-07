e93a35c1fa8db0b4712d0e0b7a213c7ed2732ee1
Paula Lima <paula.lima@mvfglobal.com>
2023-08-07 19:19:31 +0100

    Add website thumbnail with external source

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,12 +2,17 @@ import React from "react";
 import "../Styles.scss";
 
 export const Header = () => {
-	return (
-		<div className="header">
-			<img
-				src={require(`../Images/fyv_hero_front.png`)}
-				alt={"bckg-img"}
-			/>
-		</div>
-	);
+    return (
+        <div className="header">
+            <img
+                src={require(`../Images/fyv_hero_front.png`)}
+                alt={"bckg-img"}
+            />
+            <img
+                style={{ display: "none" }}
+                src={require(`../Images/website_thumbnail.png`)}
+                alt={"bckg-img"}
+            />
+        </div>
+    );
 };
