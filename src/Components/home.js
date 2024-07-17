9fe59359f98574e2b59c7e3719901dd34080f23f
Paula <paulafffl@gmail.com>
2024-07-17 17:15:20 +0100

    Clean up custom hook

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,14 +1,12 @@
 import '../Styles.scss';
 import { Header } from './header';
 import { Manifesto } from './manifesto';
-import useHeroImageLoaded from '../Hooks/useHeroImageLoaded';
 
 export const Home = () => {
-    const heroImageLoaded = useHeroImageLoaded();
     return (
         <>
             <Header />
-            {heroImageLoaded() && <Manifesto />}
+            <Manifesto />
         </>
     );
 };
--- src/Components/layout.js
+++ src/Components/layout.js
@@ -1,14 +1,12 @@
 import '../Styles.scss';
 import { Navbar } from './navbar';
 import { Footer } from './footer';
-import useHeroImageLoaded from '../Hooks/useHeroImageLoaded';
 
 export const Layout = () => {
-    const heroImageLoaded = useHeroImageLoaded();
     return (
         <>
             <Navbar />
-            {heroImageLoaded() && <Footer />}
+            <Footer />
         </>
     );
 };
