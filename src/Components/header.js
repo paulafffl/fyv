029bdeadeb8c72b038f8eff02b49dc827c20d0d3
Paula <paulafffl@gmail.com>
2024-07-17 17:14:15 +0100

    Add shimmer to Header

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Styles.scss';
+import hero_image from '../Images/fyv_hero_front.avif';
 
 export const Header = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.onload = () => {
+            setIsLoaded(true);
+        };
+        img.src = hero_image;
+    }, []);
+
     return (
         <header className="header">
-            <img src={require(`../Images/fyv_hero_front.avif`)} alt="" />
+            {!isLoaded ? (
+                <div className="header__shimmer loader-container">
+                    <div className="loader"></div>
+                </div>
+            ) : (
+                <img src={hero_image} alt="" />
+            )}
         </header>
     );
 };
